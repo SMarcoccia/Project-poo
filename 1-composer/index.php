ce021e6acc6647cd4e3d00cfc1fb8ae86050c2a6
SMARCOCCIA <seb@gmail.fr>
2024-05-02 14:42:38 +0200

    Require slugify

--- 1-composer/index.php
+++ 1-composer/index.php
@@ -1,3 +1,14 @@
+<?php 
+    require "vendor/autoload.php";
+
+    use App\classes\Member;
+    use Cocur\Slugify\Slugify;
+    
+    //require("src/classes/Member.php");
+    $member = new Member("John", "Doe");
+    //var_dump($member);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,5 +18,23 @@
 </head>
 <body>
     <h1>Projet PHP COMPOSER POO</h1>
+    <h2>Fiche détaillée d'un membre :</h2>
+    
+    <dl>
+        <dt>Prénom</dt>
+        <dd><?= $member->getFirstName() ?></dd>
+        <dt>Nom</dt>
+        <dd><?= $member->getLastName() ?></dd>
+        <dt>Date</dt>
+        <dd><?= $member->getDate()->format('Y-m-d H:i:s') ?></dd>
+    </dl>
+
+    <h2>Display d'un membre : <?= $member->printMember() ?></h2>
+
+    <h2>Test du slug par bundle externe</h2>
+    <?php
+        $slugify = new Slugify();
+        echo $slugify->slugify($member->getFirstName() . ' ' . $member->getLastName() . ' ' . $member->getDate()->format('Y-m-d H:i:s')); 
+    ?>
 </body>
 </html>
---
+++ 1-composer/src/classes/Member.php
@@ -0,0 +1,36 @@
+<?php 
+namespace App\classes;
+// Définition de l'espace de nom de la classe
+
+// Définition de la classe, le nom doit correspondre au nom du fichier
+class Member {
+  
+    // Les propriétés
+    private $firstName;
+    private $lastName;
+    private $date;
+
+    // Le constructeur
+    function __construct($firstName, $lastName) {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->date = new \DateTime();
+    }
+
+    // les setters / getters
+    public function getFirstName() {
+        return $this->firstName;
+    }
+    public function getLastName() {
+        return $this->lastName;
+    }
+    public function getDate() {
+        return $this->date;
+    }
+
+    // les autres méthodes
+    public function printMember() {
+        echo $this->date->format('Y-m-d') . ' = ' . $this->firstName . ' ' . $this->lastName;
+    }
+
+}
