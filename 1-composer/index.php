cdab1deb54d07b94d9fc292fbd43003da7a3030c
SMARCOCCIA <seb@gmail.fr>
2024-05-02 12:23:02 +0200

    Add title

--- 1-composer/index.php
+++ 1-composer/index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Projet PHP COMPOSER POO</title>
 </head>
 <body>
-    
+    <h1>Projet PHP COMPOSER POO</h1>
 </body>
 </html>
